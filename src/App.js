ba329d82445b15907333c4b38c35ac250db5d36b
Michelle Racho <rachoma12@gmail.com>
2020-10-13 16:52:32 -0400

    add skills section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import About from './components/About';
+import Skills from './components/Skills';
 import './App.scss';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 
       <main>
         <About />
+        <Skills />
       </main>
     </div>
   );

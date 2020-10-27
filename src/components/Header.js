15375a6afaff0868edfce39870aaf94babd0d255
Michelle Racho <rachoma12@gmail.com>
2020-10-27 19:19:35 -0400

    style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,42 +2,39 @@ import React, { Component } from 'react';
 import { HashLink } from 'react-router-hash-link';
 import { scrollWithOffset } from './helpers';
 import './Header.scss';
-// import headerBg from '../img/svg/header-bg.svg';
-// import { ReactComponent as SubHeader } from '../img/svg/sub-header.svg';
-import { ReactComponent as HeaderBg } from '../img/svg/header-bg.svg';
+import { ReactComponent as HeaderBgLg } from '../img/svg/header-bg-lg.svg';
+import { ReactComponent as HeaderBgMd } from '../img/svg/header-bg-md.svg';
 
 class Header extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      // media query in React
+      matches: window.matchMedia('(min-width: 768px)').matches
+    };
     this.svgRef = React.createRef(); // to access the SVG el in the render
   }
 
-  // add style to each sub-header svg path
-  // componentDidMount() {
-  //   const path = document.getElementsByClassName('path');
-
-  //   let delay = 1;
-  //   for (let i = 0; i < path.length; i++) {
-  //     this.svgRef.current.children[i].classList.add('path-stroke');
-  //     this.svgRef.current.children[i].style.animationDelay = `${delay}s`;
-  //     delay += 0.1;
-  //   }
-  // }
-
-  // add style to each header-bg svg group
   componentDidMount() {
-    // const bgGroup = document.getElementsByClassName('bg-group');
-    // console.log(bgGroup);
+    const handler = e => this.setState({ matches: e.matches });
+    window.matchMedia('(min-width: 768px)').addListener(handler);
   }
 
   render() {
+    // set bg based on screen size
+    let headerBg = this.state.matches ? (
+      <HeaderBgLg ref={this.svgRef} />
+    ) : (
+      <HeaderBgMd ref={this.svgRef} />
+    );
+
     return (
       <header className="Header" id="home">
-        <HeaderBg ref={this.svgRef} />
-        {/* <SubHeader ref={this.svgRef} /> */}
+        {headerBg}
 
         <h1 className="Header__heading">Michelle Racho</h1>
-        <p className="Header__sub-heading">A developer with an appreciation for design</p>
+        <h3 className="Header__sub-heading">A developer with an appreciation for design</h3>
+
         <HashLink className="cta" smooth scroll={scrollWithOffset} to="#about">
           Learn more
         </HashLink>

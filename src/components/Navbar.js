eeee2a8fe925e8679b82d784a9445f549e21c1c6
Michelle Racho <rachoma12@gmail.com>
2020-10-28 04:43:49 -0400

    add hamburger nav for small screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import { Link } from 'react-scroll';
 import './Navbar.scss';
 import logo from '../img/svg/logo.svg';
+import logoLg from '../img/svg/logo-lg.svg';
 
 class Navbar extends Component {
   static defaultProps = {
     links: ['About', 'Skills', 'Projects', 'Contact']
   };
 
+  constructor(props) {
+    super(props);
+    this.toggleNav = this.toggleNav.bind(this);
+  }
+
+  toggleNav() {
+    const navList = document.querySelector('.Navbar__list');
+    navList.classList.toggle('active');
+  }
+
   render() {
+    const navList = document.querySelector('.Navbar__list');
+
     const navLinks = this.props.links.map(link => (
       <li key={link} className="Navbar__item">
         <Link
@@ -29,20 +42,28 @@ class Navbar extends Component {
       <nav className="Navbar">
         <Link
           activeClass="active-section"
-          className="Navbar__logo-container Navbar__link"
+          className="Navbar__logo-link"
           to="home"
           offset={-150}
           spy={true}
           smooth={true}
           duration={1000}
         >
-          <img className="Navbar__logo" src={logo} alt="logo" />
-          <h3>Michelle Racho</h3>
+          <picture className="Navbar__logo-container">
+            <source srcSet={logo} media="(max-width: 425px)" />
+            <img className="Navbar__logo" src={logoLg} alt="logo" />
+          </picture>
         </Link>
 
-        <ul className="Navbar__list">{navLinks}</ul>
+        {/* for hamburger */}
+        <button className="toggle-nav" onClick={this.toggleNav}>
+          <i className="fas fa-bars"></i>
+        </button>
 
-        <button className="btn-dark-mode">DARK</button>
+        <ul className="Navbar__list">
+          {navLinks}
+          <button className="btn-dark-mode">DARK</button>
+        </ul>
       </nav>
     );
   }
